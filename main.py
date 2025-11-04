704481e77927248902eaa7b4a50e00d907a40841
minjaehon9 <jhmin@taxcanvas.kr>
2025-11-04 16:57:02 +0900

    feat: apis

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
+from schemas import TaxPayer
+from service import run_workflow
 import uvicorn
 
 app = FastAPI()
 
-
-@app.get("/hello")
+@app.get("/check")
 async def hello():
-    return "hello"
+    return "server running"
 
+@app.post("/register")
+async def register_taxpayer(taxpayer: TaxPayer) :
+    print(taxpayer.model_dump())
+    await run_workflow(taxpayer)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5100)
